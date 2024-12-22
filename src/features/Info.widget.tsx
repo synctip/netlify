5efdf687774c8f816ca802c7d926a655428815bc
Ilia Kamilov <iliakmlv@gmail.com>
2024-12-22 15:54:13 +0200

    init: InfoWidget

---
+++ src/features/Info.widget.tsx
@@ -0,0 +1,14 @@
+/**
+ * @file src\features\Info.widget.tsx
+ * @description Info widget component to display title, value, icon
+ * @author Ilia Kamilov <iliakmlv@gmail.com> (https://github.com/iliakamilov)
+ * @date 22/12/2024
+ * @license MIT
+ * @version 1.0.0
+ */
+
+const InfoWidget: React.FC = () => {
+  return <div>Info</div>;
+};
+
+export default InfoWidget;
