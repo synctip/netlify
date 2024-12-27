6e1976db499f004df6c9a401a1bbd698036ab3da
Ilia Kamilov <iliakmlv@gmail.com>
2024-12-27 20:00:38 +0200

    add: own designed search icon

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "@/style/Header.css";
+import SearchIcon from "@/assets/icons/SVG/search.svg";
 
 const Header: React.FC = () => {
   return (
     <header className="main">
       <h1>Synctip</h1>
       <button>
-        <img src="src/assets/icons/search.png" alt="Search" />
+        <img src={SearchIcon} />
       </button>
     </header>
   );
