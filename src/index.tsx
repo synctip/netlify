e90e6ae1469700d5ffc0cffc5167f330b1585bec
Ilia Kamilov <iliakmlv@gmail.com>
2024-12-28 22:13:26 +0200

    switched to theme modes

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
 
+// Ensure the @import statement is at the top of your CSS file
+import "@/palette/Elegant.css";
+
 const container = document.getElementById("app");
 const root = createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
